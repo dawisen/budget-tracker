8bae2a49fab6787d15e171a5f02c68e028c7ece5
gudetama <daniellewwise@gmail.com>
2021-01-23 23:41:47 -0500

    adding public files and serviceworker

---
+++ public/webpack.config.js
@@ -0,0 +1,50 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  target: 'node',
+  entry: {
+    app: '/index.js',
+    db: '/db.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget Tracker',
+      short_name: 'BTracker',
+      description: 'An application that allows you to make changes to your budget offline or on the go',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('icons/icon-192x192.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = config;
